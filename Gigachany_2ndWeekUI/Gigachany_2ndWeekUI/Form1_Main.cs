0f87150b858d02efe2e8688eb1deaa1310d36903
xodhks57 <xodhks57@naver.com>
2019-10-01 15:44:49 +0900

    추가
    그래프 추가

--- Gigachany_2ndWeekUI/Gigachany_2ndWeekUI/Form1_Main.cs
+++ Gigachany_2ndWeekUI/Gigachany_2ndWeekUI/Form1_Main.cs
@@ -15,7 +15,18 @@ namespace Gigachany_2ndWeekUI
         public Form1_Main()
         {
             InitializeComponent();
-        }
+			Graph.Titles.Add("얼굴 표정 차트").Font = new Font("나눔스퀘어", 18F, FontStyle.Bold);
+			Graph.Series["표정"].Points.Clear();
+			Graph.Series["표정"].Points.AddXY("행복", 100);
+			Graph.Series["표정"].Points.AddXY("분노", 80);
+			Graph.Series["표정"].Points.AddXY("슬픔", 40);
+			Graph.Series["표정"].Points.AddXY("무표정", 60);
+			Graph.Series["표정"].Points[0].Color = Color.FromArgb(236, 120, 121);
+			Graph.Series["표정"].Points[1].Color = Color.FromArgb(136, 182, 191);
+			Graph.Series["표정"].Points[2].Color = Color.FromArgb(236, 120, 121);
+
+
+		}
         
         #region * ButtonControls
         private void Button1_Click(object sender, EventArgs e) // 이미지 불러오기 버튼ㅇ
@@ -38,12 +49,14 @@ namespace Gigachany_2ndWeekUI
                 if (isGraph == false)
                 {
                     this.SetClientSizeCore(950, 610);
+					Graph.Visible = true;
                     isGraph = true;
                 }
                 else if (isGraph == true)
                 {
                     this.SetClientSizeCore(474, 610);
-                    isGraph = false;
+					Graph.Visible = false;
+					isGraph = false;
                 }
                 Form2_Result F2 = new Form2_Result();
                 F2.Show();
@@ -76,6 +89,8 @@ namespace Gigachany_2ndWeekUI
             this.Close();
         }
         #endregion
+
+
 
         #region * MouseControls
         private void panel1_MouseDown(object sender, MouseEventArgs e)
